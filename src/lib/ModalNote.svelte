4fcabf579ce3bdbc1b1d5583d17dc08ab97f5975
lullaby6 <lucianobrumer5@gmail.com>
2023-05-29 22:59:25 -0300

    responsive modal

--- src/lib/ModalNote.svelte
+++ src/lib/ModalNote.svelte
@@ -24,7 +24,7 @@
 <div>
     {#if showModal}
         <Modal bind:showModal>
-            <div on:click|stopPropagation class="relative shadow-md dark:bg-neutral-900 bg-white w-1/2 flex flex-col gap-2 p-4 rounded overflow-hidden">
+            <div on:click|stopPropagation class="relative shadow-md dark:bg-neutral-900 bg-white w-3/4 sm:w-3/4 md:w-2/3 lg:w-1/2 flex flex-col gap-2 p-4 rounded overflow-hidden">
                 <input bind:value={modalTitle} on:input={updateNote} type="text" placeholder="Title..." spellcheck="false" autocomplete="off" class="bg-transparent dark:text-white dark:placeholder:text-neutral-800 placeholder:italic text-2xl text-neutral-800 focus:outline-none font-semibold">
                 <textarea bind:value={modalText} on:input={updateNote} rows="15" placeholder="Text..." spellcheck="false" autocomplete="off" class="bg-transparent dark:text-white dark:placeholder:text-neutral-800 placeholder:italic text-xl text-neutral-800 focus:outline-none resize-none"></textarea>
                 <div on:click={e => {showModal = false}} class="absolute top-0 right-0 m-2">
